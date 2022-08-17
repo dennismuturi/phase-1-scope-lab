570ca9092710accedf58c6bdaafcb64afc314c66
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-17 12:31:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer ="Peter";
+
+
+function upperCaseCustomerName(){
+    customerName='BOB';
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer ='not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(leastFavorite){
+    leastFavoriteCustomer = "Mary";
+    return leastFavoriteCustomer;        
+}
